b384fd15e871ce52b34e94589dad9f8c5c25f204
Harsh Goyal <harsh.goyal@joshtechnologygroup.com>
2025-01-12 22:19:45 +0530

    Updated file names

--- app.js
+++ app.js
@@ -102,9 +102,4 @@ export default (app) => {
       }
     }
   );
-  // For more information on building apps:
-  // https://probot.github.io/docs/
-
-  // To get your app running against GitHub, see:
-  // https://probot.github.io/docs/development/
 };
